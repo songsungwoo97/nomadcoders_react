32cd9e1cc1e8675a67aa37593491990f108c738f
songsungwoo <sqwa9200@naver.com>
2023-10-08 17:55:45 +0900

    movie project

---
+++ src/routes/Detail.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const { id } = useParams();
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+    );
+    const json = await response.json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return <h1>Detail</h1>;
+}
+
+export default Detail;
